718b57a98f107eb27ef6697ddaa054631b7314b4
mohsen shiri <mohsenshiri21@yahoo.com>
2020-12-09 10:24:50 +0330

    fix Cart controller

--- Modules/Product/Http/Controllers/CartController.php
+++ Modules/Product/Http/Controllers/CartController.php
@@ -5,75 +5,70 @@ namespace Modules\Product\Http\Controllers;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Modules\Product\Models\Cart;
+use Modules\Product\Models\Product;
+use const App\Providers\HTTP_BAD_REQUEST;
+use const App\Providers\HTTP_OK;
 
 class CartController extends Controller
 {
     /**
      * Display a listing of the resource.
-     * @return Renderable
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|Renderable|\Illuminate\Http\Response
      */
     public function index()
     {
-        return view('product::index');
+        return  response(Cart::all(),HTTP_OK);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     * @return Renderable
-     */
-    public function create()
-    {
-        return view('product::create');
-    }
 
     /**
      * Store a newly created resource in storage.
      * @param Request $request
-     * @return Renderable
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|Renderable|\Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        if(Cart::create($request->toArray())){
+            return response(["message"=>trans("message.success")],HTTP_OK);
+        }
+        return response(["message"=>trans("message.error.bad")],HTTP_BAD_REQUEST);
     }
 
     /**
      * Show the specified resource.
      * @param int $id
-     * @return Renderable
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|Renderable|\Illuminate\Http\Response
      */
     public function show($id)
     {
-        return view('product::show');
+        return response( Cart::find($id), HTTP_OK);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function edit($id)
-    {
-        return view('product::edit');
-    }
+
 
     /**
      * Update the specified resource in storage.
      * @param Request $request
      * @param int $id
-     * @return Renderable
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|Renderable|\Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request,int $id)
     {
-        //
+        return response(Product::where(['id' => $id])->update($request->toArray()));
     }
 
     /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Renderable
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response|Application|Response|ResponseFactory
      */
-    public function destroy($id)
+    public function getCart(Request $request)
     {
-        //
+        $query = $request->query();
+        if ($query) {
+            return response(Cart::where($query)->get(), HTTP_OK);
+        }
+        return response(['message' => trans('message.error.bad')], HTTP_BAD_REQUEST);
+
     }
 }
